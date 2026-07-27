378aabee60055b1115c4781429b989eb3ea5d9ec
Haneesh Kumar D <haneeshkumard796@gmail.com>
2026-07-27 09:34:15 +0530

    Refactor GPIO register logic with reset handling
    Refactor GPIO register logic to include reset conditions for RGPIO_IN and RGPIO_INTS. Update always blocks to handle reset and clock events more effectively.

--- RTL/GPIO_registers.v
+++ RTL/GPIO_registers.v
@@ -14,19 +14,22 @@ module GPIO_registers(sys_clk,sys_rst,gpio_we,gpio_addr,gpio_dat_i,gpio_dat_o,gp
 	input [31:0]in_pad_i;
 	input gpio_eclk;
 	
-	wire [31:0]sel_clk;
 	reg [31:0]RGPIO_IN,RGPIO_OUT,RGPIO_OE,RGPIO_INTE,RGPIO_PTRIG,RGPIO_AUX,RGPIO_INTS,RGPIO_ECLK,RGPIO_NEC;
 	reg [1:0]RGPIO_CTRL;
 
 	assign oen_padoe_o = RGPIO_OE;
 	assign gpio_inta_o = |RGPIO_INTS;
+	
 	genvar i;
 	generate
 		for(i = 0; i <= 31; i=i+1) begin : multiplexing_out
 			assign out_pad_o[i] = RGPIO_AUX[i] ? aux_in[i] : RGPIO_OUT[i];	
 		//updating RGPIO_IN
-		always@(posedge sys_clk) begin
-			if(RGPIO_ECLK[i]) begin : update_at_eclk 
+		always@(posedge sys_clk or negedge sys_rst) begin
+			if(sys_rst == 1'b0)
+				RGPIO_IN[i] <= 1'b0;
+			
+			else if(RGPIO_ECLK[i]) begin : update_at_eclk 
 				if(RGPIO_NEC[i] == 1'b1) begin
 					if(gpio_eclk == 1'b0)
 						RGPIO_IN[i] <= in_pad_i[i];
@@ -36,13 +39,20 @@ module GPIO_registers(sys_clk,sys_rst,gpio_we,gpio_addr,gpio_dat_i,gpio_dat_o,gp
 						RGPIO_IN[i] <= in_pad_i[i];
 				end
 			end
+			
 			else begin
 				RGPIO_IN[i] <= in_pad_i[i];
 			end
 		end
-
-		always@(posedge sys_clk) begin
-			if(RGPIO_CTRL[0] == 1'b1) begin
+		//updating RGPIO_INTS
+		always@(posedge sys_clk or negedge sys_rst) begin
+			if(sys_rst == 1'b0)
+				RGPIO_INTS[i] <= 1'b0;
+			
+			else if(sys_rst== 1'b1 && gpio_we == 1'b1 && gpio_addr == 32'h1c)
+				RGPIO_INTS[i] <= gpio_dat_i[i];
+			
+			else if(RGPIO_CTRL[0] == 1'b1 && gpio_we == 1'b0 && sys_rst) begin
 				if(RGPIO_INTE[i] == 1'b1) begin
 					if(RGPIO_PTRIG[i] == 1'b1) begin
 						if(in_pad_i[i]==1'b1 && RGPIO_IN[i]==1'b0)
@@ -60,19 +70,17 @@ module GPIO_registers(sys_clk,sys_rst,gpio_we,gpio_addr,gpio_dat_i,gpio_dat_o,gp
 
 	//Read-Write acess for APB Master
 	always@(posedge sys_clk or negedge sys_rst) begin
-		if(!sys_rst) begin
-			RGPIO_IN <= 32'd0;
+		if(sys_rst == 1'b0) begin
 			RGPIO_OUT <= 32'd0;
 			RGPIO_OE <= 32'd0;
 			RGPIO_INTE <= 32'd0;
 			RGPIO_PTRIG <= 32'd0;
 			RGPIO_AUX <= 32'd0;
 			RGPIO_CTRL <= 2'd0;
-			RGPIO_INTS <= 32'd0;
 			RGPIO_ECLK <= 32'd0;
 			RGPIO_NEC <= 32'd0;
 		end
-		else if(gpio_we)	
+		else if(sys_rst == 1'b1 && gpio_we == 1'b1)	
 			case(gpio_addr)
 				32'h4 : RGPIO_OUT <= gpio_dat_i;
 				32'h8 : RGPIO_OE <= gpio_dat_i;
@@ -80,72 +88,6 @@ module GPIO_registers(sys_clk,sys_rst,gpio_we,gpio_addr,gpio_dat_i,gpio_dat_o,gp
 				32'h10 : RGPIO_PTRIG <= gpio_dat_i;
 				32'h14 : RGPIO_AUX <= gpio_dat_i;
 				32'h18 : RGPIO_CTRL <= gpio_dat_i;
-				32'h1c : RGPIO_INTS <= gpio_dat_i;
-				32'h20 : RGPIO_ECLK <= gpio_dat_i;
-				32'h24 : RGPIO_NEC <= gpio_dat_i;
-			endcase
-		else
-			case(gpio_addr)
-				32'h0 : gpio_dat_o <= RGPIO_IN;
-				32'h4 : gpio_dat_o <= RGPIO_OUT;
-				32'h8 :  gpio_dat_o <= RGPIO_OE;
-				32'hc : gpio_dat_o <= RGPIO_INTE;
-				32'h10 : gpio_dat_o <= RGPIO_PTRIG;
-				32'h14 : gpio_dat_o <= RGPIO_AUX;
-				32'h18 : gpio_dat_o <= RGPIO_CTRL;
-				32'h1c : gpio_dat_o <= RGPIO_INTS;
-				32'h20 : gpio_dat_o <= RGPIO_ECLK;
-				32'h24 : gpio_dat_o <= RGPIO_NEC;
-			endcase	
-	end	
-endmodule						RGPIO_IN[i] <= in_pad_i[i];
-				end
-			end
-			else begin
-				RGPIO_IN[i] <= in_pad_i[i];
-			end
-		end
-
-		always@(posedge sys_clk) begin
-			if(RGPIO_CTRL[0] == 1'b1) begin
-				if(RGPIO_INTE[i] == 1'b1)
-					if(RGPIO_PTRIG[i] == 1'b1) begin
-						if(in_pad_i[i]==1'b1 && RGPIO_IN[i]==1'b0)
-							RGPIO_INTS[i] <= 1'b1;
-					end
-					else if(RGPIO_PTRIG[i] == 1'b0) begin
-						if(in_pad_i[i]==1'b0 && RGPIO_IN[i]==1'b1)
-							RGPIO_INTS[i] <= 1'b1;
-					end	
-			end
-			else
-				RGPIO_INTS[i] <= 1'b0;
-		end
-	end 
-	endgenerate
-
-	//Read-Write acess for APB Master
-	always@(posedge sys_clk or negedge sys_rst) begin
-		if(!sys_rst) begin
-			RGPIO_OUT <= 32'd0;
-			RGPIO_OE <= 32'd0;
-			RGPIO_INTE <= 32'd0;
-			RGPIO_PTRIG <= 32'd0;
-			RGPIO_AUX <= 32'd0;
-			RGPIO_CTRL <= 2'd0;
-			RGPIO_INTS <= 32'd0;
-			RGPIO_ECLK <= 32'd0;
-			RGPIO_NEC <= 32'd0;
-		end
-		else if(gpio_we)	
-			case(gpio_addr)
-				32'h4 : RGPIO_OUT <= gpio_dat_i;
-				32'h8 : RGPIO_OE <= gpio_dat_i;
-				32'hc : RGPIO_INTE <= gpio_dat_i;
-				32'h10 : RGPIO_PTRIG <= gpio_dat_i;
-				32'h14 : RGPIO_AUX <= gpio_dat_i;
-				32'h18 : RGPIO_CTRL <= gpio_dat_i;
-				32'h1c : RGPIO_INTS <= gpio_dat_i;
 				32'h20 : RGPIO_ECLK <= gpio_dat_i;
 				32'h24 : RGPIO_NEC <= gpio_dat_i;
 			endcase
