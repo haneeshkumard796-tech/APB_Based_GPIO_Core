bc6a50d0b2dbf73446769e6e16753a3282ebd71b
Haneesh Kumar D <haneeshkumard796@gmail.com>
2026-09-02 19:35:05 +0530

    Add eclk generation to IO_pad_interface

--- Test Bench/IO_pad_interface.sv
+++ Test Bench/IO_pad_interface.sv
@@ -3,12 +3,18 @@ interface IO_pad_interface(input bit clock);
 	wire [31:0] io_pad;
 	logic eclk;
 
+	initial begin
+		eclk = 1'b0;
+		forever #40 eclk = ~eclk;
+	end
+	
 	clocking DRV_CB@(posedge clock);
 		inout io_pad;
+		input eclk;
 	endclocking	
 	clocking MON_CB@(posedge clock);
 		input io_pad;
-	endclocking	
+	endclocking
 
 	modport DRV_MP(clocking DRV_CB);
 	modport MON_MP(clocking MON_CB);	
